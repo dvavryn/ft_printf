c42f8c7bab8c2381161fadc1d4107da622ed1d76
Dominic Vavryn <dvavryn@c3r2p5.42vienna.com>
2025-05-09 13:58:19 +0200

    fin

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: dvavryn <dvavryn@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 22:17:10 by dvavryn           #+#    #+#             */
-/*   Updated: 2025/05/09 13:02:38 by dvavryn          ###   ########.fr       */
+/*   Updated: 2025/05/09 13:55:09 by dvavryn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define FT_PRINTF_H
 
 # include <stdarg.h>
-# include "./libft/libft.h"
+# include <unistd.h>
 
 int	ft_printf(const char *format, ...);
 int	ft_putchar(int c);
@@ -24,5 +24,6 @@ int	ft_putunbr(unsigned int n);
 int	ft_putnbr_hex(int c, unsigned int n);
 int	ft_padress(void *p);
 int	ft_putulong_hex(unsigned long n);
+size_t	ft_strlen(const char *s);
 
 #endif
--- ft_strlen.c
+++ ft_strlen.c
@@ -1,43 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dvavryn <dvavryn@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/22 13:26:24 by dvavryn           #+#    #+#             */
-/*   Updated: 2025/04/28 13:25:04 by dvavryn          ###   ########.fr       */
+/*   Created: 2025/04/22 12:16:08 by dvavryn           #+#    #+#             */
+/*   Updated: 2025/05/09 13:55:30 by dvavryn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "ft_printf.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlen(const char *s)
 {
-	size_t	d;
-	size_t	s;
 	size_t	i;
 
-	d = ft_strlen(dst);
-	s = ft_strlen(src);
 	i = 0;
-	if (size == 0)
-		return (s);
-	if (size <= d)
-		return (size + s);
-	while (d + i < size - 1 && src[i] != '\0')
+	while (s[i] != '\0')
 	{
-		dst[d + i] = src[i];
 		i++;
 	}
-	dst[d + i] = '\0';
-	return (d + s);
+	return (i);
 }
 /* #include <stdio.h>
 int main(void)
 {
-	char *s1 = " I am a string";
-	char s2[40] = "Hello World";
-	ft_strlcat(s2, s1, 20);
-	printf("%s", s2);
+	printf("%zu", ft_strlen("Hello World"));
 } */
