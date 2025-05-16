1a336284c1cc2ee6f76f7c5169184dabfbcb9d55
Dominic Vavryn <dvavryn@c3r3p5.42vienna.com>
2025-05-16 18:33:42 +0200

    change

--- ft_putchar.c
+++ ft_putchar.c
@@ -6,7 +6,7 @@
 /*   By: dvavryn <dvavryn@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 18:24:59 by dvavryn           #+#    #+#             */
-/*   Updated: 2025/05/09 13:02:58 by dvavryn          ###   ########.fr       */
+/*   Updated: 2025/05/16 18:33:31 by dvavryn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 int	ft_putchar(int c)
 {
-	write(1, &c, 1);
+	unsigned char	uc;
+
+	uc = (unsigned char)c;
+	write(1, &uc, 1);
 	return (1);
 }
